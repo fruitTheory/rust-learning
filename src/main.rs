c50b637c7af132999490310f9afabe2a018c7c40
Derwin <derwinprince61@gmail.com>
2023-11-13 23:54:11 -0500

    quick test

--- src/main.rs
+++ src/main.rs
@@ -2,26 +2,25 @@
 // Comment
 fn main(){
 
-    // print!("Hello\n");
-    // println!("Hello {}", 42);
-    // println!("Hello {}", "World");
-    // println!("Hello {} {}", "World", 60);
-    // println!("Hello {1} {0}", "World", 60);
-
     println!("Hello {noun} {number}", noun = "World", number = 60);
     println!("{:b} {:X} {}", 1, 10, 10); // binary , hex, decimal
 
     println!("{num:0>4}", num = 3);
     println!("{num:0>fill$}", num = 3, fill = 4);
 
-    // #[allow(dead_code)] // disable `dead_code` which warn against unused module
-    // struct Structure(i32);
-    // println!("This struct `{}` won't print...", Structure(3));
-
-    let numb: u8 = 255;
     let numb: u8 = 0xFF;
-
-    println!("{numb}");
     println!("{:X} {0}", numb);
+
+    let i = test(9);
+    println!("printing i: {i}");
+
+
+
+}
+
+fn test(a: i32) -> i32{
+
+    let a = a;
+    return a;
 
 }
